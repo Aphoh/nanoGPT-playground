a1f73142166aab43a16d95be39301144f6a06ac1
William Arnold <will748@gmail.com>
2024-01-02 14:19:46 -0800

    tweaks

--- config/finetune_shakespeare.py
+++ config/finetune_shakespeare.py
@@ -5,7 +5,6 @@ eval_interval = 5
 eval_iters = 40
 wandb_log = False # feel free to turn on
 wandb_project = 'shakespeare'
-wandb_run_name = 'ft-' + str(time.time())
 
 dataset = 'shakespeare'
 init_from = 'gpt2-xl' # this is the largest GPT-2 model
