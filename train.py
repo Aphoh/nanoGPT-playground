f8be095b803ac172418f6be701568ecf60065e17
William Arnold <will748@gmail.com>
2024-01-03 10:44:17 -0800

    Add OUT_DIR env override

--- train.py
+++ train.py
@@ -77,6 +77,7 @@ compile = True # use PyTorch 2.0 to compile the model to be faster
 # -----------------------------------------------------------------------------
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
 exec(open('configurator.py').read()) # overrides from command line or config file
+out_dir = os.environ.get('OUT_DIR', out_dir) # override from env var
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
 # -----------------------------------------------------------------------------
 
